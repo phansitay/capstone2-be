4e746981fd567a428d83a4ebf08773c1c8e7002a
phansitay <82353942+phansitay@users.noreply.github.com>
2023-05-13 21:00:53 +0700

    update

--- src/entities/bmi-kid.ts
+++ src/entities/bmi-kid.ts
@@ -11,28 +11,38 @@ import { WithTimestamp } from './withTimestamp';
 import { SuggestSchedule } from './suggest-schedule';
 import { UserEntity as Users } from './user.entity';
 import { Baby } from './baby';
+import { IsBoolean, IsDate, IsNumber, IsString } from "class-validator";
 
 @Index('bmi_kid_pkey', ['id'], { unique: true })
 @Entity('bmi_kid', { schema: 'public' })
 export class BmiKid extends WithTimestamp {
-  @ApiProperty()
-  @Column('integer', { primary: true, name: 'id' })
-  id: number;
+  // @ApiProperty()
+  // @Column('integer', { primary: true, name: 'id' })
+  // id: number;
 
   @ApiProperty()
-  @Column('numeric', { name: 'weight', nullable: true })
+  @IsString()
+  @Column('character varying', { name: 'weight', nullable: true })
   weight: string | null;
 
   @ApiProperty()
-  @Column('numeric', { name: 'height', nullable: true })
+  @IsString()
+  @Column('character varying', { name: 'height', nullable: true })
   height: string | null;
 
   @ApiProperty()
-  @Column('numeric', { name: 'bmi', nullable: true })
+  @IsString()
+  @Column('character varying', { name: 'bmi', nullable: true })
   bmi: string | null;
 
   @ApiProperty()
-  @Column('date', { name: 'start_time', nullable: true })
+  @IsString()
+  @Column('character varying', { name: 'content', nullable: true })
+  content: string | null;
+
+  @ApiProperty()
+  @IsString()
+  @Column('character varying', { name: 'start_time', nullable: true })
   startTime: string | null;
 
   // @ApiProperty()
